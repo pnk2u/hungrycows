0d1dc417ee625fc73af916fd78db42b01b7bf0cb
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-10-17 14:20:42 +0200

    Initial Commit (1.21.2)

---
+++ src/main/java/de/pnku/hungrycows/renderer/HungryCowRenderState.java
@@ -0,0 +1,11 @@
+package de.pnku.hungrycows.renderer;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
+
+@Environment(EnvType.CLIENT)
+public class HungryCowRenderState extends LivingEntityRenderState {
+    public float headAngle;
+    public float neckAngle;
+}
