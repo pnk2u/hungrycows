067771435a30a9f8deffb280e7d4c227b25dfd24
pnk2u <159317511+pnk2u@users.noreply.github.com>
2025-02-24 04:08:06 +0100

    Fix remap error

--- src/main/java/de/pnku/hungrycows/mixin/compat/bovinesandbuttercups/client/renderer/entity/MoobloomRendererMixin.java
+++ src/main/java/de/pnku/hungrycows/mixin/compat/bovinesandbuttercups/client/renderer/entity/MoobloomRendererMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(MoobloomRenderer.class)
 public class MoobloomRendererMixin {
 
-    @Inject(method = "extractRenderState(Lhouse/greenhouse/bovinesandbuttercups/content/entity/Moobloom;Lhouse/greenhouse/bovinesandbuttercups/client/renderer/entity/model/state/MoobloomRenderState;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/AgeableMobRenderer;extractRenderState(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/client/renderer/entity/state/LivingEntityRenderState;F)V", shift = At.Shift.AFTER), remap = false)
+    @Inject(method = "extractRenderState(Lhouse/greenhouse/bovinesandbuttercups/content/entity/Moobloom;Lhouse/greenhouse/bovinesandbuttercups/client/renderer/entity/model/state/MoobloomRenderState;F)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/AgeableMobRenderer;extractRenderState(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/client/renderer/entity/state/LivingEntityRenderState;F)V", shift = At.Shift.AFTER))
     public void extractRenderState(Moobloom moobloom, MoobloomRenderState moobloomRenderState, float partialTicks, CallbackInfo ci) {
         ((IHungryCows) moobloomRenderState).hungrycows$setHeadAngle(((IHungryCows) moobloom).hungrycows$getHeadAngle(partialTicks));
         ((IHungryCows) moobloomRenderState).hungrycows$setNeckAngle(((IHungryCows) moobloom).hungrycows$getNeckAngle(partialTicks));
